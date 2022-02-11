2fbdf725dcac94f83e0a84cbe140574d498459a7
Ansuree-Reddy <anusreekedika20@gmail.com>
2022-02-11 16:58:58 -0600

    Variables.java

---
+++ Variables.java
@@ -0,0 +1,21 @@
+package com.java.practice;
+
+public class Variables {
+	public static void main(String[] args) {
+		int myNumber = 77;
+		short myShort = 888;
+		long myLong = 9999;
+		double myDouble = 6.666;
+		float myFloat = 323.4f;
+		char myChar = 'y';
+		boolean myBoolean = false;
+		byte myByte = 127;
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+	}
+}
