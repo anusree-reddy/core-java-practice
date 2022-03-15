8bb5db54bc8d143c56b51243a6b06f1a5386f1b8
Anusree Reddy Kedika <w10127348@usm.edu>
2022-03-15 01:07:10 -0500

    Constructor

--- src/com/anu/practice/ConstructorExample.java
+++ src/com/anu/practice/ConstructorExample.java
@@ -1,10 +1,31 @@
 package com.anu.practice;
+class Machine{
+	private String name;
+	private int code;
+	public Machine() {
+		this("Arnie",0);
+		
+		System.out.println("Constructor running!");
+	}
+	public Machine(String name) {
+		this(name,0);
+		
+		System.out.println("Second Constructor running!");
+	}
+		public Machine(String name,int code) {
+			System.out.println("Third Constructor running!");
+			this.name = name;
+			this.code = 519;
+		}
+	}
+	
 
 public class ConstructorExample {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+  public static void main(String[] args) {
+		
+Machine machine1 = new Machine();
+Machine machine2 = new Machine("Krishna");
+Machine machine3 = new Machine("Nikki",10);
 	}
 
 }
